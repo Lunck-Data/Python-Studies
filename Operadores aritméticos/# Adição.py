390b5325fb0323380a643c6dd4c4b5b8cfa4e837
Luan Vieira Teixeira <luan.trader98@gmail.com>
2025-03-30 16:29:56 -0300

    1-Estudos-Python

---
+++ Operadores aritméticos/# Adição.py
@@ -0,0 +1,44 @@
+# Adição
+print(1 + 1)
+# 2
+
+# Subtração
+print(10 - 2)
+# 8
+
+# Multiplicação
+print(4 * 3)
+# 12
+
+# Divisão
+print(12 / 3)
+# 4.0
+
+# Divisão inteira
+print(12 // 2)
+# 6
+
+# Módulo
+print(10 % 3)
+# 1
+
+# Exponenciação
+print(2 ** 3)
+# 8
+
+# Precedência de operadores
+
+print(10 - 5 * 2)
+# 0
+
+print((10 - 5) * 2)
+# 10
+
+print(10 ** 2 * 2)
+# 200
+
+print(10 ** (2 * 2))
+# 10.000
+
+print(10 / 2 * 4)
+# 20.0
