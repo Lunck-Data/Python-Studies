ea97f05617a5488628e6911edc7c2a372b97ce4d
Luan Vieira Teixeira <luan.trader98@gmail.com>
2025-03-31 21:27:28 -0300

    Todas aulas de operadores

---
+++ Tipos-de-operadores-com-python/Operadores de atribuição.py
@@ -0,0 +1,62 @@
+# Atribuição simples "="
+
+saldo = 500
+
+print(saldo)
+# 500
+
+# Atribuição com adição
+
+saldo = 500
+saldo += 200
+
+print(saldo)
+# 700
+
+# Atribuição com subtração
+
+saldo = 500
+saldo -= 200
+
+print(saldo)
+# 300
+
+# Atribuição com multiplicação
+
+saldo = 500
+saldo *= 2
+
+print(saldo)
+# 1000
+
+# Atribuição com divisão
+
+saldo = 500
+saldo /= 2
+
+print(saldo)
+# 250.0
+
+# Divisão inteira
+
+saldo = 500
+saldo //= 2
+
+print(saldo)
+# 250
+
+# Atribuição com módulo
+
+saldo = 500
+saldo %= 480
+
+print(saldo)
+# 20
+
+# Atribuição com exponenciação
+
+saldo = 80
+saldo **= 2
+
+print(saldo)
+# 6400
---
+++ Tipos-de-operadores-com-python/Operadores de comparação.py
@@ -0,0 +1,36 @@
+# Igualdade
+saldo = 450
+saque = 200
+
+print(saldo == saque)
+# False
+
+# Diferança
+saldo = 450
+saque = 200
+print (saldo != saque)
+# True
+
+# Maior que / maior ou igual
+
+saldo = 450
+saque = 200
+print(saldo > saque)
+# True
+
+saldo = 450
+saque = 200
+print(saldo >= saque)
+# True
+
+# Menor que / menor ou igual
+
+saldo = 450
+saque = 200
+print(saldo < saque)
+# False
+
+saldo = 450
+saque = 200
+print(saldo <= saque)
+# False
